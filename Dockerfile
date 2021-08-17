e684fc0575ae4924701aee36d7d5d902e3c6eeef
JayeshGSalgaonkar-Palo <jsalgaonkar@palo-it.com>
2021-08-18 07:26:11 +0800

    performance optimization

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,24 @@
 FROM maven:3-jdk-8-alpine
 
+# Create env variables
 ENV mcr mcr
 ENV url url
 
 #RUN addgroup app && adduser -S -G app appuser
 #USER appuser
 
+# Create working directory
 RUN mkdir /app
 WORKDIR /app
+
+# Maven dependencies caching
+COPY pom.xml /app
+RUN mvn test-compile -DskipTests
+
+# Copy code / files
 COPY . /app
 
+# Access rights
 RUN chmod u+x maven_runner.sh
 
 ENTRYPOINT /app/maven_runner.sh $mcr $url
-
--- maven_runner.sh
+++ maven_runner.sh
@@ -1,13 +1,29 @@
  #!/usr/bin/env bash
 
- echo "START: Running tests..."
+ mcr=$1
+ url=$2
 
- mvn test -T 3.0C "-Dkarate.options=--tags $1" -DbaseURL=$2 -Dtest=TestRunnerParallel
+ echo "START: Running functional tests..."
+
+ mvn test -T 3.0C "-Dkarate.options=--tags $mcr" -DbaseURL=$url -Dtest=TestRunnerParallel
 
  if [[ $? -ne 0 ]] ; then
    echo "FINISH: There are test failures, failing build..."
    echo "Exiting with code 1."
-   exit 1
+ else
+   echo "FINISH: All tests passed!"
+   echo "Exiting with code 0."
+   exit 0
+ fi
+
+
+  echo "START: Running non-functional tests..."
+
+ mvn clean test-compile -DbaseURL=$url gatling:test
+
+ if [[ $? -ne 0 ]] ; then
+   echo "FINISH: There are test failures, failing build..."
+   echo "Exiting with code 1."
  else
    echo "FINISH: All tests passed!"
    echo "Exiting with code 0."
