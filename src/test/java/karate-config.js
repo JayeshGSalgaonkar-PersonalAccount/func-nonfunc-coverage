d79f6b9a7eb1286182f4cddb7b3cf681d7326fb2
JayeshGSalgaonkar-Palo <jsalgaonkar@palo-it.com>
2022-02-09 10:54:43 +0800

    4000 cc user on Prod

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -13,7 +13,7 @@ function fn() {
    var config = {}
 
     if (env == 'dev'){
-        config.baseURL = karate.properties['baseURL'] || 'https://devma.ntuc.org.sg/api/'
+        config.baseURL = karate.properties['baseURL'] || 'https://apimdev.ntuc.org.sg/api/'
         config.xAPIKEY = 'bm1wSW50ZXJuYWxTZXJ2aWNl'
         }
     else if(env == 'preprod'){
@@ -21,13 +21,15 @@ function fn() {
         config.xAPIKEY = 'MjIwYWFlMTItYzg0NC00NTU4LWIyZjAtNjc2Y2Y5ZmMzNzUw'
         }
     else if(env == 'uat'){
-        config.baseURL = karate.properties['baseURL'] || 'https://uatma.ntuc.org.sg/api/'
+        config.baseURL = karate.properties['baseURL'] || 'https://apimuat.ntuc.org.sg/api/'
         config.xAPIKEY = 'MzI2YjUyNzYtOGY2MS00MjhlLWFjNTAtOGQ1ZjU0YmE2MjI2'
         }
-    else  if(env == 'prod')
-        config.baseURL = karate.properties['baseURL'] || 'https://ma.ntuc.org.sg/api/'
+    else  if(env == 'prod'){
+        config.baseURL = karate.properties['baseURL'] || 'https://apim.ntuc.org.sg/api/'
+        config.xAPIKEY = 'MzI2YjUyNzYtOGY2MS00MjhlLWFjNTAtOGQ1ZjU0YmE2MjI2'
+        }
     else {
-         config.baseURL = karate.properties['baseURL'] || 'https://devma.ntuc.org.sg/api/'
+         config.baseURL = karate.properties['baseURL'] || 'https://apimdev.ntuc.org.sg/api/'
          config.xAPIKEY = 'bm1wSW50ZXJuYWxTZXJ2aWNl'
          }
 //  By default run in dev environment
