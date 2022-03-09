96f00a47d8cf8ce7bbdc7f34599531024b4099d5
JayeshGSalgaonkar-Palo <jsalgaonkar@palo-it.com>
2022-03-09 13:36:23 +0800

    protection_Service load trigger

--- src/test/java/ntuc_memberportal/LoadSimulation_Total.scala
+++ src/test/java/ntuc_memberportal/LoadSimulation_Total.scala
@@ -13,13 +13,17 @@ class LoadSimulation_Total extends Simulation {
   val deal_Service: ScenarioBuilder = scenario("deal-service").exec(karateFeature("classpath:ntuc_memberportal/FeatureFiles/deal-service/performance.feature"))
   val notification_Service: ScenarioBuilder = scenario("notification-service").exec(karateFeature("classpath:ntuc_memberportal/FeatureFiles/notification-service/performance.feature"))
   val payment_Service: ScenarioBuilder = scenario("payment-service").exec(karateFeature("classpath:ntuc_memberportal/FeatureFiles/payment-service/performance.feature"))
+  val protection_Service: ScenarioBuilder = scenario("protection-service").exec(karateFeature("classpath:ntuc_memberportal.FeatureFiles.protection-service/case.feature"))
+
+
 
   setUp(
     user_Service.inject(constantConcurrentUsers(8000).during(1500)),
     member_Service.inject(constantConcurrentUsers(8000).during(1500)),
     deal_Service.inject(constantConcurrentUsers(8000).during(1500)),
     notification_Service.inject(constantConcurrentUsers(8000).during(1500)),
-    payment_Service.inject(constantConcurrentUsers(8000).during(1500))
+    payment_Service.inject(constantConcurrentUsers(8000).during(1500)),
+    protection_Service.inject(constantConcurrentUsers(8000).during(1500))
   )
 }
 
